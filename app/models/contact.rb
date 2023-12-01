c2e832033992dac60276fdecc5ce0c9b3a13d4be
Joshua Yee <joshyee16@gmail.com>
2023-12-01 11:04:07 -0800

    create new contacts using the rails console, CRUD around

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,9 +1,2 @@
-class Contact
-  attr_accessor :first_name, :last_name, :phone_number, :email
-  def initialize(opts)
-    @first_name = opts[:first_name]
-    @last_name = opts[:last_name]
-    @phone_number = opts[:phone_number]
-    @email = opts[:email]
-  end
+class Contact < ApplicationRecord
 end
